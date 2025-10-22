2a0c2f1bdb90ea15d8c995f46551432b0117fd6e
dattu <dattusrinu1122@gmail.com>
2025-10-22 14:34:11 +0530

    progress

---
+++ 8_Authentication/8.2_API_Keys/env_api_key.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Depends, Header, HTTPException
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    api_key : str
+
+    class Config:
+        env_file : str ='.env'
+
+settings = Settings() 
+app = FastAPI()
+
+
+def get_api_key(api_key : str = Header(...)):
+    if api_key != settings.api_key:
+        raise HTTPException(status_code=400, detail='Unauthorised')
+    return api_key
+
+
+@app.get('/get-data')
+def get_data(api_key: str = Depends(get_api_key)):
+    return {'output':'Access Granted!'}
